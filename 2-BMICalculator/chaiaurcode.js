66208d32b39a86370cc116a7da75dd9d5def95bf
Arjun Veer <arjunveer7066@gmail.com>
2025-01-28 20:38:03 +0530

    project 4 complete

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -1,6 +1,5 @@
 // const weight = document.querySelector('#weight');
 //this usecase will give you empty
-
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function (e) {
@@ -9,19 +8,27 @@ form.addEventListener('submit', function (e) {
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
+
+
+  if (height < 0 || height === '' || isNaN(height)) {
+    results.innerHTML = ` Please give valid height ${height}`;
+  } else if (weight < 0 || weight === '' || isNaN(weight)) {
+    results.innerHTML = ` Please give valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    if (bmi < 18.5) {
+      results.innerHTML = `<span> Under Weight ${bmi}</span>`;
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      results.innerHTML = `<span> Normal Weight ${bmi}</span>`;
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      results.innerHTML = `<span> Overweight ${bmi}</span>`;
+    }
+  }
+
 });
 
-if (height < 0 || height === '' || isNaN(height)) {
-  results.innerHTML = ` Please give valid height ${height}`;
-} else if (weight < 0 || weight === '' || isNaN(weight)) {
-  results.innerHTML = ` Please give valid weight ${weight}`;
-} else {
-  const bmi = weight / (height * height).toFixed(2);
-  //show the result
-  results.innerHTML = `<span> ${bmi}</span>`;
-}
 
-console.log(height);
 
 // const form = document.querySelector('form');
 // // this usecase will give you empty
